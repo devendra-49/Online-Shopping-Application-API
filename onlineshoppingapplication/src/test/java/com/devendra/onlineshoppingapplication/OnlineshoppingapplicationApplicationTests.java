58665bb02a7b841e397163259a6d3ec83ad3ffb2
Devendra-9970 <dmule5578@gmail.com>
2025-09-19 12:36:48 +0530

    First Commit

---
+++ onlineshoppingapplication/src/test/java/com/devendra/onlineshoppingapplication/OnlineshoppingapplicationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.devendra.onlineshoppingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshoppingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
