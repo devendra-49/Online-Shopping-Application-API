cf9813455194c98259ecee154ddd79c22336b7ae
Devendra-9970 <dmule5578@gmail.com>
2025-09-19 12:15:17 +0530

    First Commit

--- onlineshoppingapplication/src/test/java/edu/jspiders/employeemanagementsystem/OnlineshoppingapplicationApplicationTests.java
+++ onlineshoppingapplication/src/test/java/edu/jspiders/employeemanagementsystem/OnlineshoppingapplicationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devendra.onlineshoppingapplication;
+package edu.jspiders.employeemanagementsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
